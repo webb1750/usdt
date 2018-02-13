f667807063652fe1412a3a71de7009a892f0a034
Jon Webb <jonathan.webb@treasury.gov>
2018-02-13 15:42:04 -0500

    minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function() {
     gulp.src('scss/usdt.scss')
@@ -10,6 +11,15 @@ gulp.task('sass', function() {
 gulp.task('watch', function(){
     gulp.watch('scss/*.scss', ['sass']);
 });
+
+gulp.task('minify-css', () => {
+    return gulp.src('css/*.css')
+    .pipe(cleanCSS({debug: true}, (details) => {
+        console.log(`${details.name}: ${details.stats.originalSize}`);
+        console.log(`${details.name}: ${details.stats.minifiedSize}`);
+      }))
+      .pipe(gulp.dest('css'));
+   });
 gulp.task('default', ['sass'], function() {
-    gulp.watch('scss/*.scss', ['sass']);
-})
+    gulp.watch('scss/*.scss', ['sass'], ['minify-css']);
+});
